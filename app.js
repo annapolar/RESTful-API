30712ad592629cb997c5f0a0914d623563618bd1
AnnaHuang <anna.huang@oriente.com>
2020-07-18 17:01:16 -0600

    get articles

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+//connect db
 mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser:true});
 
 // create schema
@@ -21,10 +22,18 @@ const articleSchema = {
     title: String,
     content: String
 }
-//create modal (usually singlur and captial format)
+//create modal (usually singlur and capital format)
 const Article = mongoose.model("Article", articleSchema);
 
-//TODO
+app.get("/articles", function(req, res){
+    Article.find(function(err, foundArticles){
+        if (!err){
+            res.send(foundArticles)
+        } else {
+            res.send(err)
+        }
+    })
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
