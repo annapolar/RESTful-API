8e65ed92de3ebc6bb49e2dd59729df2daf7c3d6b
AnnaHuang <anna.huang@oriente.com>
2020-07-19 13:26:04 -0600

    Using express chained route handler

--- app.js
+++ app.js
@@ -25,43 +25,43 @@ const articleSchema = {
 // create modal (usually singlur and capital format)
 const Article = mongoose.model("Article", articleSchema);
 
-// Get Articles
-// app.get("route", function(req, res){})
-app.get("/articles", function(req, res){
-    Article.find(function(err, foundArticles){
-        if (!err){
-            res.send(foundArticles)
-        } else {
-            res.send(err)
-        }
-    })
-})
-// Post Articles
-app.post("/articles", function(req, res){
+// Using express chained route handler
+// http://expressjs.com/en/5x/api.html#router
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
     // console.log(req.body.title)
     // console.log(req.body.content)
     const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
-    })
-    newArticle.save(function(err){
-        if(!err){
-            res.send("Successfully added a new article")
-        } else {
-            res.send(err)
-        }
+      title: req.body.title,
+      content: req.body.content,
     });
-})
-//Delete (if no condition in the deleteMarny call back function, it will delete all)
-app.delete("/articles", function(req, res){
-    Article.deleteMany(function(err){
-        if(!err){
-            res.send("Delete all articles successfully")
-        } else {
-            res.send(err)
-        }
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Successfully added a new article");
+      } else {
+        res.send(err);
+      }
     });
-})
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Delete all articles successfully");
+      } else {
+        res.send(err);
+      }
+    });
+  });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
